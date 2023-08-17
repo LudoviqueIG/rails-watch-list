f2dbb9503ce38cce9e170fb608e828ab4f62c3d4
Ruth ITOUA GATSE <ruthitoua@yahoo.fr>
2023-08-18 00:44:37 +0200

    register

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,27 +1,24 @@
 class BookmarksController < ApplicationController
-
   before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: [:new, :create]
-
+  
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @list = List.find(params[:list_id])
-    @movie = Movie.find(params[:movie_id])
-    @bookmark = Bookmark.new(params[:bookmark_id])
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.movie = @movie
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   def destroy
-    @bookmarks.destroy
+    @bookmark.destroy
     redirect_to list_path(@bookmark.list), status: :see_other
   end
 
@@ -35,9 +32,6 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
-  def set_list
-    @list = List.find(params[:list_id])
-  end
 
 end
 
